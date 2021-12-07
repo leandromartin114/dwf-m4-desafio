5362d047419e1f9c5e87cbe3063e4c5dfb3b2b93
Leandro <leandrom.roldan@gmail.com>
2021-12-07 13:07:51 -0300

    Componentes y estética terminados

--- componentes/header.js
+++ componentes/header.js
@@ -6,11 +6,11 @@ function insertHeader(el) {
 		<ul class="menu__ul">
 			<li class="menu__li"><a href="./porftolio.html">Portfolio</a></li>
 			<li class="menu__li"><a href="./servicios.html">Servicios</a></li>
-			<li class="menu__li"><a href="./contacto.html">Contactos</a></li>
+			<li class="menu__li"><a href="./contacto.html">Contacto</a></li>
 		</ul>
 	</div>
 	<header class="header">
-		<img src="./images/logo-w.png" alt="logo" class="header__logo" />
+		<a href="./index.html"><img src="./images/logo-w.png" alt="logo" class="header__logo" /></a>
 		<img src="./images/menu-w.png" alt="menu" class="header__open" />
 		<ul class="header__menu-desktop">
 			<li class="header__menu-desktop-li">
---
+++ servicios.js
@@ -0,0 +1,58 @@
+function addService(params) {
+	const template = document.querySelector(".template");
+	const content = document.querySelector(".main-section__content");
+	template.content.querySelector(".main-section__title").textContent =
+		params.title;
+	template.content.querySelector(".main-section__text").textContent =
+		params.description;
+	template.content.querySelector(".main-section__image").src = params.img;
+	const clone = document.importNode(template.content, true);
+	content.appendChild(clone);
+}
+function getService() {
+	return fetch(
+		"https://cdn.contentful.com/spaces/0vpr854aizab/environments/master/entries?access_token=Z2oHgxxwZ7fNpp4_3ZNbYUvOaxwv1nZ6_5ZZBk4Q0uE"
+	)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			const items = data.items;
+			const asset = data.includes.Asset;
+			const newItems = items.filter((i) => {
+				return i.sys.contentType.sys.id === "service";
+			});
+			const collection = newItems.map((i) => {
+				return {
+					title: i.fields.titulo,
+					description: i.fields.descripcion,
+					imageId: i.fields.imagen.sys.id,
+				};
+			});
+			collection.forEach((e) => {
+				const id = getImage(e.imageId, asset);
+				e.img = "https:" + id.fields.file.url;
+			});
+			return collection;
+		});
+}
+
+function getImage(id, array) {
+	const img = array.find((f) => {
+		return f.sys.id == id;
+	});
+	return img;
+}
+function main() {
+	const headerContainer = document.querySelector(".header-section");
+	const footerContainer = document.querySelector(".footer");
+	insertHeader(headerContainer);
+	openMenu();
+	insertFooter(footerContainer);
+	getService().then((works) => {
+		for (const iterator of works) {
+			addService(iterator);
+		}
+	});
+}
+main();
